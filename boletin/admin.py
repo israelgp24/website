305a62c1ea08d856ea2d8a4febf4fd5072b35e1a
Israel Edgar Gonzales Palomino <igonzalesp@uni.pe>
2019-06-10 23:21:36 -0500

    refactor: change to format pep8

--- boletin/admin.py
+++ boletin/admin.py
@@ -5,14 +5,10 @@ from .forms import RegModelForm
 from .models import Registrado
 
 class AdminRegistrado(admin.ModelAdmin):
-    list_display=["email","nombre","timestamp"]
-    form=RegModelForm
-    list_filter=["timestamp"]
-    list_editable=["nombre"]
-    search_fields=["email","nombre"]
-    
-##    class Meta:
-##        model= Registrado
-        
+    list_display = ["email","nombre","timestamp"]
+    form = RegModelForm
+    list_filter = ["timestamp"]
+    list_editable = ["nombre"]
+    search_fields = ["email","nombre"]    
 
 admin.site.register(Registrado,AdminRegistrado)
